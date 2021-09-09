5745337bdb34a5cb6ab33a95e911891804acb70f
JinnYoo <89617354+JinnYoo@users.noreply.github.com>
2021-09-09 17:50:32 +0900

    Add files via upload

---
+++ glamDev/js/index.js
@@ -0,0 +1,21 @@
+//index.js
+
+$document.ready(function(){
+    alert(1)
+});
+
+/*
+ var name = prompt('Input your name!!');
+      document.write('Welcome ' + name);
+      
+      $(function(){
+          $('.content').scroll(function(){
+              console.log('hello');
+          });
+      });      
+
+
+      $('.window').animate( { scrollTop : $(selector).offset().top }, 500 
+
+      $('nav').css('border','1px solid blue')
+*/
